404e0c24cc40c62f47a340bb1a5dddefb027bf90
lvkbots <senbla001@gmail.com>
2025-02-06 16:57:37 +0100

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from flask import Flask, request
-import logging
 
 # Configuration
 TOKEN = os.environ.get('7184666905:AAFd2arfmIFZ86cp9NNVp57dKkH6hAVi4iM')
@@ -16,18 +15,21 @@ server = Flask(__name__)
 def send_welcome(message):
     welcome_text = (
         "Bonjour. Je m'appelle Mustafa Zulu 🖥️\n\n"
-        "Mon équipe a développé un algorithme pour calculer le prochain coef dans Aviator "
+        "Mon équipe et moi-même avons développé un algorithme de programme qui calcule le prochain coef dans le jeu Aviator "
         "avec une précision de *99,997%* ✅\n\n"
         "Nous vous apprendrons à utiliser ce programme pour *gagner 120 000 dès aujourd'hui* 💸💰\n\n"
-        "Écrivez-moi et je vous donnerai le programme 🎁"
+        "Écrivez-moi et je vous donnerai le programme 🎁\n"
+        "👉 [@moustaphalux](https://t.me/moustaphalux) 👈"
     )
+    
+    bot.send_message(message.chat.id, welcome_text, parse_mode="Markdown", disable_web_page_preview=True)
     
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("✅ Écrire ✉️", url="https://t.me/moustaphalux"))
     markup.add(InlineKeyboardButton("✅ Critiques 💲", url="https://t.me/moustaphalux"))
     markup.add(InlineKeyboardButton("💻 Fonctionnement du programme", url="https://t.me/moustaphalux"))
     
-    bot.send_message(message.chat.id, welcome_text, parse_mode="Markdown", reply_markup=markup)
+    bot.send_message(message.chat.id, "Sélectionnez une option ci-dessous :", reply_markup=markup)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def webhook():
