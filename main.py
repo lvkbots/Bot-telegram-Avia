fbc96bd590516a4bb475ddcce31eaf505c566efb
lvkbots <senbla001@gmail.com>
2025-02-06 16:14:00 +0100

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-# main.py
 import os
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import time
 
 TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TOKEN)
+
+# Configuration pour éviter les conflits
+bot.remove_webhook()
+time.sleep(1)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     welcome_text = (
         "Bonjour. Je m'appelle Mustafa Zulu 🖥️\n\n"
-         "Mon équipe et moi-même avons développé un algorithme de programme qui calcule le prochain coef dans le jeu Aviator "
-         "avec une précision de *99,997%* ✅\n\n"
-         "Nous vous apprendrons à utiliser ce programme pour *gagner 120 000 dès aujourd'hui* 💸💰\n\n"
-         "Écrivez-moi et je vous donnerai le programme 🎁"
+        "Mon équipe et moi-même avons développé un algorithme de programme qui calcule le prochain coef dans le jeu Aviator "
+        "avec une précision de *99,997%* ✅\n\n"
+        "Nous vous apprendrons à utiliser ce programme pour *gagner 120 000 dès aujourd'hui* 💸💰\n\n"
+        "Écrivez-moi et je vous donnerai le programme 🎁"
     )
     
     markup = InlineKeyboardMarkup()
@@ -24,4 +28,4 @@ def send_welcome(message):
     bot.send_message(message.chat.id, welcome_text, parse_mode="Markdown", reply_markup=markup)
 
 print("Bot démarré!")
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, interval=0)
